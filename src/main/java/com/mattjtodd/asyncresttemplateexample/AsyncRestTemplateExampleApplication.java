764ac2b7fd6432e96d4c06f3665ae0c24cb34845
mattjtodd <matt.j.todd@gmail.com>
2017-07-19 07:58:59 +0100

    Adde netty

--- src/main/java/com/mattjtodd/asyncresttemplateexample/AsyncRestTemplateExampleApplication.java
+++ src/main/java/com/mattjtodd/asyncresttemplateexample/AsyncRestTemplateExampleApplication.java
@@ -3,20 +3,24 @@ package com.mattjtodd.asyncresttemplateexample;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.omg.CORBA.TIMEOUT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.web.client.AsyncRestTemplate;
 
 @SpringBootApplication
 public class AsyncRestTemplateExampleApplication {
 
+	private static final int TIMEOUT = 3000;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncRestTemplateExampleApplication.class, args);
 	}
 
-	@Bean
+	@Bean("HttpCommons")
 	public AsyncRestTemplate getAsyncRestTemplate() {
 
 		HttpComponentsAsyncClientHttpRequestFactory asyncRequestFactory = new HttpComponentsAsyncClientHttpRequestFactory();
@@ -24,14 +28,20 @@ public class AsyncRestTemplateExampleApplication {
 		return new AsyncRestTemplate(asyncRequestFactory);
 	}
 
+	@Bean("Netty")
+	public AsyncRestTemplate getNettyAsyncRestTemplate() {
+		Netty4ClientHttpRequestFactory factory = new Netty4ClientHttpRequestFactory();
+		factory.setReadTimeout(TIMEOUT);
+		return new AsyncRestTemplate(factory);
+	}
+
 	@Bean
 	public CloseableHttpAsyncClient asyncHttpClient() {
-		int connectionTimeout = 3000;
 
 		IOReactorConfig ioReactorConfig = IOReactorConfig
 				.custom()
-				.setConnectTimeout(connectionTimeout)
-				.setSoTimeout(connectionTimeout)
+				.setConnectTimeout(TIMEOUT)
+				.setSoTimeout(TIMEOUT)
 				.setIoThreadCount(5)
 				.build();
 
